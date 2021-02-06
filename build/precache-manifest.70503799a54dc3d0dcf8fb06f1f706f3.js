72f07adc6e370c068ba55a9579dbf0bff885f7b4
vanderson-henrique <vhbenedito@gmail.com>
2021-02-06 17:42:25 -0300

    Atualização para inserir um repositório para o projeto Trybe Wallet

---
+++ build/precache-manifest.70503799a54dc3d0dcf8fb06f1f706f3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba959890981d2cfde6e0ae468bf486b",
+    "url": "/app-trybe-wallet/index.html"
+  },
+  {
+    "revision": "787002d8bfe94cc6765b",
+    "url": "/app-trybe-wallet/static/css/main.4a26f6c0.chunk.css"
+  },
+  {
+    "revision": "f195488dc805a31b9bdd",
+    "url": "/app-trybe-wallet/static/js/2.152886ca.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/app-trybe-wallet/static/js/2.152886ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "787002d8bfe94cc6765b",
+    "url": "/app-trybe-wallet/static/js/main.b6309336.chunk.js"
+  },
+  {
+    "revision": "b164bd5dde281227b3f2",
+    "url": "/app-trybe-wallet/static/js/runtime-main.f5f31bba.js"
+  },
+  {
+    "revision": "3e7d8b5eb62039994eb0c17b238cdeef",
+    "url": "/app-trybe-wallet/static/media/btn-editar.3e7d8b5e.svg"
+  },
+  {
+    "revision": "971f63595c1c24bb750198aefd9fad40",
+    "url": "/app-trybe-wallet/static/media/btn-excluir.971f6359.svg"
+  },
+  {
+    "revision": "ba775a0a94cf16a2ab7cf633b5be531c",
+    "url": "/app-trybe-wallet/static/media/carteira.ba775a0a.svg"
+  }
+]);
